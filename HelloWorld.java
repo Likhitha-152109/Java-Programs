cf57fc9fcf103619f99f69280fd1b588e79dfb21
likhitha <likhithalucky666@gmail.com>
2026-01-26 22:42:18 +0530

    Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,8 @@
 import java.util.*;
-public class HelloWorld
+class HelloWorld
 {
-	public static void main(String[] args)
-	{
-		System.out.println("Hello World!");
-	}
+public static void main(String[] args)
+{
+System.out.println("Hello Woorld!");
 }
-		
+}
