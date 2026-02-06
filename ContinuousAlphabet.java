455da87e8d710702b42682dd29268cf17a60ee9f
Likhitha Bezavada <likhithabezavada15@gmail.com>
2026-02-06 13:02:41 +0530

    Continuous alphabet pattern

--- ContinuousAlphabet.java
+++ ContinuousAlphabet.java
@@ -2,12 +2,12 @@ class ContinuousAlphabet
 {
 	public static void main(String[] args)
 	{
-		int num=1;
+		char ch='A';
 		for(int i=1;i<=4;i++)
 		{
 			for(int j=1;j<=i;j++)
 			{
-				System.out.print(num++);
+				System.out.print(ch++);
 			}
 			System.out.println();
 		}
