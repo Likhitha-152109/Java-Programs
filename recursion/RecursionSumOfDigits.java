0eee2feb37da8d4cb9e9fcb2eae754c1b29789ef
Likhitha Bezavada <likhithabezavada15@gmail.com>
2026-02-07 20:59:43 +0530

    Recursion sum of digits

---
+++ recursion/RecursionSumOfDigits.java
@@ -0,0 +1,16 @@
+class RecursionSumOfDigits
+{
+	public static void main(String[] args)
+	{
+		int n=123;
+		System.out.println("Sum="+sum(n));
+	}
+	static int sum(int n)
+	{
+		if(n==0)
+		{
+			return 0;
+		}
+		return n%10+sum(n/10);
+	}
+}
