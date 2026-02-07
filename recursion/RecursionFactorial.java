99bce898d9f05a97b203b007a07e4ca92372474f
Likhitha Bezavada <likhithabezavada15@gmail.com>
2026-02-07 20:37:51 +0530

    Recursion factorial

---
+++ recursion/RecursionFactorial.java
@@ -0,0 +1,16 @@
+class RecursionFactorial
+{
+	public static void main(String[] args)
+	{
+		int n=5;
+		System.out.println("Factorial="+fact(n));
+	}
+	static int fact(int n)
+	{
+		if(n==0)
+		{
+			return 1;
+		}
+		return n*fact(n-1);
+	}
+}
