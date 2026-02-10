0aed734f6f0e6851f0cab1a7a8532d20f00c2910
Likhitha Bezavada <likhithabezavada15@gmail.com>
2026-02-10 06:17:19 +0530

    Recursion printing of numbers

---
+++ recursion/RecursionPrintNumbers.java
@@ -0,0 +1,17 @@
+class RecursionPrintNumbers
+{
+	public static void main(String[] args)
+	{
+		int n=5;
+		print(n);
+	}
+	static void print(int n)
+	{
+		if(n==0)
+		{
+			return;
+		}
+		print(n-1);
+		System.out.print(n+" ");
+	}
+}
