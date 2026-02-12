ae4f61ecf38bf26065e301e7260a4469c76e6738
Likhitha Bezavada <likhithabezavada15@gmail.com>
2026-02-12 21:43:23 +0530

    Print numbers

---
+++ PrintNumbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class PrintNumbers
+{
+	public static void main(String[] args)
+	{
+		int n;
+		Scanner s=new Scanner(System.in);
+		for(int i=1;i<=n;i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
