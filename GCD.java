f6993be78c9eefb7e214b4eaae6991650855569e
Likhitha Bezavada <likhithabezavada15@gmail.com>
2026-02-14 20:04:01 +0530

    Gcd/hcf using loops

--- GCD.java
+++ GCD.java
@@ -9,7 +9,7 @@ class GCD
 		System.out.println("Enter second number:");
 		int b=s.nextInt();
 		int gcd=1;
-		for(int i=0;i<=a && i<=b;i++)
+		for(int i=1;i<=a && i<=b;i++)
 		{
 			if(a%i==0 && b%i==0)
 			{
