8d6a4a3b23ee5b4123e6489977fac354ade2e178
Likhitha Bezavada <likhithabezavada15@gmail.com>
2026-02-22 12:43:50 +0530

    removing zeroes and add it to last

---
+++ arrays/RemovingZeroes.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class RemovingZeroes
+{
+	public static void main(String[] args)
+	{
+		int arr[]={1,0,2,0,0,3,4,5};
+		int i;
+		for(i=0;i<arr.length;i++)
+		{
+			if(arr[i]!=0)
+			{
+				System.out.print("Array after removing zeroes and add it to last:"+arr[i]+" ");
+			}
+		}
+		for(i=0;i<arr.length;i++);
+		{
+			if(arr[i]==0)
+			{
+				System.out.print(arr[i]+" ");
+			}
+		}
+	}
+}
