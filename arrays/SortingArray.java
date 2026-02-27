bb2fad0623095e14c5af1ef6ce36117a09744418
Likhitha Bezavada <likhithabezavada15@gmail.com>
2026-02-27 12:54:11 +0530

    Sorting array using selection sort

---
+++ arrays/SortingArray.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class SortingArray
+{
+	public static void main(String[] args)
+	{
+		int arr[]={5,2,9,1,3};
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			int min=i;
+			for(int j=i;j<arr.length;j++)
+			{
+				if(arr[j]<arr[min])
+				{
+					min=j;
+				}
+			}
+			int temp=arr[i];
+			arr[i]=arr[min];
+			arr[min]=temp;
+		}
+		for(int x:arr)
+		{
+			System.out.print(x);
+		}
+	}
+}
