f4b7af1a60e92585318f37c261805f74bd8759ef
Likhitha Bezavada <likhithabezavada15@gmail.com>
2026-03-03 12:41:45 +0530

    Sum of the 2D array

---
+++ arrays/Sum2DArray.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Sum2DArray
+{
+	public static void main(String[] args)
+	{
+		int arr[][]={
+			{1,2,3},
+				{4,5,6}
+		};
+		int sum=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0;j<arr[i].length;j++)
+			{
+				sum=sum+arr[i][j];
+			}
+		}
+		System.out.print("Sum="+sum);
+	}
+}
+				
