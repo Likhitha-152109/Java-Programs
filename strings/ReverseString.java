22dd5715460dbee985be9c49f1957c0ba61c57b5
Likhitha Bezavada <likhithabezavada15@gmail.com>
2026-03-05 17:42:43 +0530

    Reverse astring using loops

---
+++ strings/ReverseString.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ReverseString
+{
+	public static void main(String[] args)
+	{
+		String s="hello";
+		String reversed="";
+		for(int i=s.length()-1;i>=0;i--)
+		{
+			reversed=reversed+s.charAt(i);
+		}
+		System.out.println("Reversed string:"+reversed);
+	}
+}
