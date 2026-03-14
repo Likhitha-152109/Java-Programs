2a4977bf0f385bcbe7ce74e47887748d7c05a4f1
Likhitha Bezavada <likhithabezavada15@gmail.com>
2026-03-14 22:45:51 +0530

    Reverse the string

---
+++ strings/Reverse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Reverse
+{
+	public static void main(String[] args)
+	{
+		String str="hello";
+		String rev="";
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			rev=rev+str.charAt(i);
+		}
+		System.out.println(rev);
+	}
+}
