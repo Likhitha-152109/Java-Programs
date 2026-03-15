03061501b07f64e8c0a9228e1ccf5f084b2fea40
Likhitha Bezavada <likhithabezavada15@gmail.com>
2026-03-15 22:20:04 +0530

    Remove spaces

---
+++ strings/RemoveSpace.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class RemoveSpace
+{
+	public static void main(String[] args)
+	{
+		String str="java programming";
+		str=str.replace(" ","");
+		System.out.println(str);
+	}
+}
