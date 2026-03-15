9289fe7176f28167846b8826bafb989261103018
Likhitha Bezavada <likhithabezavada15@gmail.com>
2026-03-15 22:00:27 +0530

    Palindrome string

---
+++ strings/PalindromeString.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class PalindromeString
+{
+	public static void main(String[] args)
+	{
+		String str="hello";
+		String rev="";
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			rev=rev+str.charAt(i);
+		}
+		if(str.equals(rev))
+		{
+			System.out.println("Palindrome");
+		}
+		else
+		{
+			System.out.println("Not a palindrome");
+		}
+	}
+}
+			
