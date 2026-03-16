d19eca8b69a5c39fa6312e93d6f8ac093c2f4c74
Likhitha Bezavada <likhithabezavada15@gmail.com>
2026-03-16 17:09:28 +0530

    Remove duplicate characters in a string

---
+++ strings/RemoveDuplicates.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class RemoveDuplicates
+{
+	public static void main(String[] args)
+	{
+		String s="banana";
+		String result="";
+		for(int i=0;i<s.length();i++)
+		{
+			char ch=s.charAt(i);
+			if(result.indexOf(ch)==-1)
+			{
+				result=result+ch;
+			}
+		}
+		System.out.println(result);
+	}
+}
