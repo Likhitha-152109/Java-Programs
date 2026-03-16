bc092b108fcb15076a0303fda76d48018a13bee6
Likhitha Bezavada <likhithabezavada15@gmail.com>
2026-03-16 17:43:08 +0530

    Lowercase to uppercase

---
+++ strings/LowerToUpper.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class LowerToUpper
+{
+	public static void main(String[] args)
+	{
+		String s="java programming";
+		String result=s.toUpperCase();
+		System.out.println(result);
+	}
+}i
